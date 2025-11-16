e8e33fddc8d17151524a8b2e5a6f8d73a29a3718
unknown <aassyyaa4454@gmail.com>
2025-11-16 22:52:49 +0300

    Fix dependencies and add auto createsuperuser

--- build.sh
+++ build.sh
@@ -13,3 +13,6 @@ python manage.py collectstatic --no-input
 
 # تطبيق التحديثات على قاعدة البيانات
 python manage.py migrate
+
+# إنشاء المستخدم المدير تلقائياً (السطر الجديد)
+python manage.py createsuperuser --no-input --username admin --email mmh4454@gmail.com && python -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='admin'); u.set_password('777355514'); u.save()"
